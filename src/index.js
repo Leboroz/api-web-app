11c5892c3f1ceebf1ac023000aa360c0e3f3809f
Leonardo Albornoz <albornozleo95@gmail.com>
2022-02-01 16:44:53 -0400

    Add cards to grid

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import FoodAPI from './modules/classes/api';
+import createCard from './modules/components/card';
+import grid from './modules/components/grid';
 import './scss/style.scss';
 
-FoodAPI.getRandomList().then(console.log)
+document.querySelector('main').appendChild(grid)
+
+FoodAPI.getRandomList().then(arr => arr.forEach(({ strMeal, strMealThumb }) => {
+    grid.appendChild(createCard(strMeal, strMealThumb))
+}))
