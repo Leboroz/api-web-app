497982986c2b5ce88ea0dbeea4fced1e199c9105
Leonardo Albornoz <albornozleo95@gmail.com>
2022-02-01 18:35:01 -0400

    Add event to heart button

--- src/modules/components/card.js
+++ src/modules/components/card.js
@@ -6,19 +6,28 @@ export default function createCard(mealName, strMealImage) {
     mealImage.className = 'img-wrapper'
     mealImage.innerHTML = `<img src="${strMealImage}">`
 
-    const mealTitle = document.createElement('div')
-    mealTitle.className = 'card-body'
-    mealTitle.innerHTML = `
+    const mealCardBody = document.createElement('div')
+    mealCardBody.className = 'card-body'
+    mealCardBody.innerHTML = `
     <article class="card-header">
         <h2 class="card-title">${mealName}</h2>
         <div class="like-button">
             <i class="fas fa-heart"></i>
-            <span class="counter">
+            <span><span class="counter">0</span> likes</span>
         </div>
     </article>
     <button class="btn" type="button">Comments</button>
     <button class="btn" type="button">Reservations</button>`
 
-    mealCard.append(mealImage, mealTitle)
+    mealCard.append(mealImage, mealCardBody)
+
+    const likeButton = mealCardBody.querySelector('.like-button')
+    likeButton.addEventListener('click', (e) => {
+        const { target } = e
+        if(target.className === "fas fa-heart") {
+            target.classList.add('pop')
+            setTimeout(() => {target.classList.remove('pop')}, 500)
+        }
+    })
     return mealCard
 }
