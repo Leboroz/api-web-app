e7bb9d3ac05674f8f1ed08c8a4893d01508a2649
gdumani <gdumani@gmail.com>
2022-02-02 12:49:03 -0600

    display comment  on main

--- src/modules/displayComment.js
+++ src/modules/displayComment.js
@@ -1,17 +1,21 @@
 const displayComment = (meal) => {
-  console.log(meal)
-  // const commentModal = document.createElement('div');
-  // commentModal.className='modal-popup'
-  // const mealObj=meal.meals[0];
-  // commentModal.innerHTML=`
-  // <img src="${mealObj.strMealThumb}" alt="Meal image">
-  // <h1>${mealObj.strMeal}</h1>
-  // <p>${mealobj.strInstructions}</p>
-  // <h3>Commets</h3>
-  // <p> ...list of comments</p>
-  // <h3>Add a Comment>
-  // <input type="text" id='comment-mame' placeholder="Your Name">
-  // <input type="text" id='comment-content' placeholder="Your Insights">
-  // `
-}
+  meal.then((mealObj) => {
+    const selectMeal = mealObj.meals[0];
+    console.log(mealObj)
+    const commentModal = document.createElement('div');
+    commentModal.className='modal-popup'
+    commentModal.innerHTML=`
+    <img src="${selectMeal.strMealThumb}" alt="Meal image">
+    <h1>${selectMeal.strMeal}</h1>
+    <p>${selectMeal.strInstructions}</p>
+    <h3>Commets</h3>
+    <p> ...list of comments</p>
+    <h3>Add a Comment>
+    <input type="text" id='comment-mame' placeholder="Your Name">
+    <input type="text" id='comment-content' placeholder="Your Insights">
+    `
+    const main = document.querySelector('main');
+    main.appendChild(commentModal);
+  })
+  }
 export default displayComment;
