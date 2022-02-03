00a67738a7f559fa65ba21cdead1f73cdfb78c48
Leonardo <albornozleo95@gmail.com>
2022-02-03 00:02:15 -0400

    Add like fetching to card and set id to grid

--- src/modules/components/card.js
+++ src/modules/components/card.js
@@ -1,35 +1,47 @@
-export default function createCard(mealName, strMealImage, idMeal) {
-  const mealCard = document.createElement('div');
-  mealCard.className = 'card';
-  mealCard.id = `meal-${idMeal}`;
+import InvolvementAPI from '../classes/involvementapi'
 
-  const mealImage = document.createElement('figure');
-  mealImage.className = 'img-wrapper';
-  mealImage.innerHTML = `<img src="${strMealImage}">`;
+export default function createCard(mealName, strMealImage, idMeal, likes) {
+  const mealCard = document.createElement('div')
+  mealCard.className = 'card'
+  mealCard.id = `meal-${idMeal}`
 
-  const mealCardBody = document.createElement('div');
-  mealCardBody.className = 'card-body';
+  const mealImage = document.createElement('figure')
+  mealImage.className = 'img-wrapper'
+  mealImage.innerHTML = `<img src="${strMealImage}">`
+
+  const mealCardBody = document.createElement('div')
+  mealCardBody.className = 'card-body'
   mealCardBody.innerHTML = `
     <article class="card-header">
         <h2 class="card-title">${mealName}</h2>
         <div class="like-button">
             <i class="fas fa-heart"></i>
-            <span><span class="counter">0</span> likes</span>
+            <span><span class="likes-counter">${
+              likes.length ? likes[0].likes : 0
+            }</span> likes</span>
         </div>
     </article>
-    <button class="btn" type="button">Comments</button>`;
+    <button class="btn" type="button">Comments</button>`
 
-  mealCard.append(mealImage, mealCardBody);
+  mealCard.append(mealImage, mealCardBody)
 
-  const likeButton = mealCardBody.querySelector('.like-button');
+  const likeButton = mealCardBody.querySelector('.like-button')
   likeButton.addEventListener('click', (e) => {
-    const { target } = e;
+    const { target } = e
     if (target.className === 'fas fa-heart') {
-      target.classList.add('pop');
+      target.classList.add('pop')
+      InvolvementAPI.setLike(idMeal + '').then(() => {
+        InvolvementAPI.getLikes().then((likes) => {
+          mealCardBody.querySelector('.likes-counter').innerHTML = likes.filter(
+            ({ item_id }) => item_id === idMeal
+          )[0].likes
+        })
+      })
+
       setTimeout(() => {
-        target.classList.remove('pop');
-      }, 500);
+        target.classList.remove('pop')
+      }, 500)
     }
-  });
-  return mealCard;
+  })
+  return mealCard
 }
--- src/modules/components/grid.js
+++ src/modules/components/grid.js
@@ -1,19 +1,25 @@
-import itemsCounter from '../functions/counter';
+import itemsCounter from '../functions/counter'
 
-const grid = document.createElement('div');
-grid.className = 'grid';
+const grid = document.createElement('div')
+grid.className = 'grid'
+grid.id = 'grid'
 
 const childrenCounter = (e) => {
-  document.querySelector('#counter').innerHTML = itemsCounter(
-    e.target.parentNode,
-  );
-};
+  if (e.target.parentNode.id === 'grid') {
+    document.querySelector('#counter').innerHTML = itemsCounter(
+      e.target.parentNode
+    )
+  }
+}
 
 const childrenCounterRemove = (e) => {
-  document.querySelector('#counter').innerHTML = itemsCounter(e.target.parentNode) - 1;
-};
+  if (e.target.parentNode.id === 'grid') {
+    document.querySelector('#counter').innerHTML =
+      itemsCounter(e.target.parentNode) - 1
+  }
+}
 
-grid.addEventListener('DOMNodeRemoved', childrenCounterRemove);
-grid.addEventListener('DOMNodeInserted', childrenCounter);
+grid.addEventListener('DOMNodeRemoved', childrenCounterRemove)
+grid.addEventListener('DOMNodeInserted', childrenCounter)
 
-export default grid;
+export default grid
