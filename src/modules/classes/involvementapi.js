f9ae5f72147556225b80d1e76ecc2cffe2fe0d61
Leonardo <albornozleo95@gmail.com>
2022-02-03 00:01:21 -0400

    Fix URL and add like methods

--- src/modules/classes/involvementapi.js
+++ src/modules/classes/involvementapi.js
@@ -1,11 +1,26 @@
 export default class InvolvementAPI {
-  static URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+  static URL =
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/'
 
-  static APP_ID = 'zjDNOoNc6riMp48wH4yB/';
+  static APP_ID = 'zjDNOoNc6riMp48wH4yB/'
 
-  static LIKES = 'likes/';
+  static LIKES = 'likes/'
 
-  static COMMENTS = 'comments';
+  static COMMENTS = 'comments'
 
   static getComments
+
+  static getLikes() {
+    return fetch(this.URL + this.APP_ID + this.LIKES).then((res) => res.json())
+  }
+
+  static setLike(id) {
+    return fetch(this.URL + this.APP_ID + this.LIKES, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ item_id: id }),
+    })
+  }
 }
