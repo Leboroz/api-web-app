dbb60e037ef701c31ed43ffcd26b4adbed16ad35
gdumani <gdumani@gmail.com>
2022-02-04 15:56:26 -0600

    enable comment counter

--- src/modules/components/commentsToList.js
+++ src/modules/components/commentsToList.js
@@ -1,6 +1,7 @@
 export default function commentsToList(comments) {
-  let list = '';
+  let list = 'no comments';
   if (comments.length) {
+    list = `<h5>${comments.length}  comments</h5>`;
     comments.forEach((c) => {
       list += `<h4>${c.username}<span class='date'>${c.creation_date}</span></h4>
     <p class='comment'>${c.comment}</p>`;
