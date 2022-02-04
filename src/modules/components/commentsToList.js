70824635e5c5b7bfec5b7d4dc0499df68ecb18d7
gdumani <gdumani@gmail.com>
2022-02-04 09:47:19 -0600

    fix linters

--- src/modules/components/commentsToList.js
+++ src/modules/components/commentsToList.js
@@ -1,6 +1,5 @@
 export default function commentsToList(comments) {
   let list = '';
-  console.log(comments.lenth);
   if (comments.length) {
     comments.forEach((c) => {
       list += `<h4>${c.username}<span class='date'>${c.creation_date}</span></h4>
